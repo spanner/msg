3bfece6b6d867017bdb33b3a766fe0d26a942cf0
William Ross <will@spanner.org>
2013-03-27 12:05:24 +0000

    group and receiver setters in Sending

--- app/models/msg/sending.rb
+++ app/models/msg/sending.rb
@@ -8,5 +8,20 @@ module Msg
     has_many :envelopes, :dependent => :destroy
     accepts_nested_attributes_for :envelopes
 
+    def self.add_receiving_class(klass, options)
+      key = klass.to_s.underscore
+      define_method  :"#{key}_groups=" do |scope_name|
+        send :"#{key}_receiver_ids=", klass.send(scope_name.to_sym)
+      end
+      define_method :"#{key}_receiver_ids=" do |ids|
+        send :"#{key}_receivers=", klass.find(ids)
+      end
+      define_method :"#{key}_receivers=" do |receivers|
+        receivers.each do |receiver|
+          envelopes.build(:receiver => receiver)
+        end
+      end
+    end
+
   end
 end
