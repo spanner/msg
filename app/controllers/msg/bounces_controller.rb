e98ba95c28519e47f6b6e755bca7e94089a42a99
William Ross <will@spanner.org>
2013-04-23 17:43:43 +0100

    a few debug lines

--- app/controllers/msg/bounces_controller.rb
+++ app/controllers/msg/bounces_controller.rb
@@ -2,23 +2,21 @@ require 'open-uri'
 
 module Msg
   class BouncesController < ::ApplicationController
-    respond_to :json
-    
     before_filter :read_json_body
 
     # When the subscription is first set up they will also POST us a SubscriptionConfirmation,
     # upon which we have to GET the given url to confirm that we are able to follow orders.
     #
     def create
-      data = ActiveSupport::JSON.decode(request.body)
+      Rails.logger.warn "!!! requeset.env: #{requeset.env.inspect}"
       
       if request.env['x-amz-sns-message-type'] == 'SubscriptionConfirmation'
-        response = HTTParty.get(data['SubscribeURL'])
+        response = HTTParty.get(@data['SubscribeURL'])
         head :ok
 
       else
-        mail_data = data['mail']
-        bounce_data = data['bounce']
+        mail_data = @data['mail']
+        bounce_data = @data['bounce']
         recipients = [bounce_data['bouncedRecipients']].flatten
         envelope = Msg::Envelope.find_by_email_id(mail_data['messageId'])
         recipients.each do |recipient_data|
@@ -40,10 +38,11 @@ module Msg
   protected
     
     # Amazon sends its SNS notifications as json data with a text/plain content-type, which 
-    # rails really hates. Here we read the json body if it hasn't already been read.
+    # rails really doesn't get on. Here, with any luck, we go back and retrieve the json body from the request.
     #
     def read_json_body
-    
+      @data = ActiveSupport::JSON.decode(request.body)
+      Rails.logger.warn "!!! bounce notification body: #{@data.inspect}"
     end
   
   end
